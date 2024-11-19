3ed1c745585613b60123d3cfd6fb1e87eefcaf0e
elijah <cuiyeqing960904@gmail.com>
2024-11-19 18:34:38 +0300

    building client

--- src/Service/Pinger.php
+++ src/Service/Pinger.php
@@ -1,6 +1,25 @@
 <?php
 
-class Pinger
-{
+namespace App\Service;
 
+use GRPC\Pinger\PingerInterface;
+use Spiral\RoadRunner\GRPC;
+use GRPC\Pinger\PingRequest;
+use GRPC\Pinger\PingResponse;
+
+final class Pinger implements PingerInterface
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient
+    ) {
+    }
+
+    public function ping(GRPC\ContextInterface $ctx, PingRequest $in): PingResponse
+    {
+        $statusCode = $this->httpClient->get($in->getUrl())->getStatusCode();
+
+        return new PingResponse([
+            'status_code' => $statusCode
+        ]);
+    }
 }
