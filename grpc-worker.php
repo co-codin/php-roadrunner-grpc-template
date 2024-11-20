53b646f79cd27f10a0b5a8c82727fbe027e34752
elijah <cuiyeqing960904@gmail.com>
2024-11-20 19:43:28 +0300

    built

--- grpc-worker.php
+++ grpc-worker.php
@@ -4,13 +4,12 @@ use GRPC\Pinger\PingerInterface;
 use Spiral\RoadRunner\GRPC\Server;
 use Spiral\RoadRunner\Worker;
 use Symfony\Component\HttpClient\HttpClient;
+use App\Service\Pinger;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$server = new Server(null, [
-    'debug' => false, // optional (default: false)
-]);
+$server = new Server();
 
-$server->registerService(PingerInterface::class, new \App\Service\Pinger(HttpClient::create()));
+$server->registerService(PingerInterface::class, new Pinger(HttpClient::create()));
 
 $server->serve(Worker::create());
